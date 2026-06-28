6cf157317bf6aba53f3b50486af4b2b2ec09d180
S-pen-cer <20260814@mywhitecliffe.com>
2026-06-29 10:40:14 +1200

    Add files via upload
    This runs into an error stating that "status" needs to be defined, meaning setting it to "pending" doesn't remove the necessity for inputting status. This doesn't seem to fulfil th required output. Might need to change data structure because of this.

--- Assessment 2/test5.py
+++ Assessment 2/test5.py
@@ -1,6 +1,6 @@
 
-class employee_register:
-    Cost = 0
+class Registry:
+    total_cost = 0
     order_count = 0
     registry_info = []
     receipt = []
@@ -12,7 +12,7 @@ class employee_register:
         self.name = name
         self.id = id
         self.status = "pending"
-        employee_register.registry_info = [self.date, self.name, self.id, self.status]
+        Registry.registry_info = [self.date, self.name, self.id, self.status]
 
     def get_order(self):
         item = input("Please enter product name")
@@ -25,36 +25,37 @@ class employee_register:
         if quantity < 0:
             print("Please enter a valid quantity")
         cost = price * quantity
-        employee_register.Cost = cost
-        employee_register.receipt = [item, employee_register.Cost]
+        Registry.total_cost = cost
+        Registry.receipt = [item, Registry.total_cost]
 
     def update_status(self):
-        if employee_register.Cost < 500:
+        if Registry.total_cost < 500:
             self.status = "approved"
-        print(employee_register.Cost, self.status)
-        employee_register.registry_info = [self.date, self.name, self.id, self.status]
+        print(Registry.total_cost, self.status)
+        Registry.registry_info = [self.date, self.name, self.id, self.status]
 
     def order_stats(self):
         if self.status == "approved":
-            employee_register.approval_counter += 1
+            Registry.approval_counter += 1
         if self.status == "pending":
-            employee_register.approval_counter += 1
+            Registry.approval_counter += 1
         if self.status == "declined":
-            employee_register.decline_counter += 1
-        print(f"The number of approved orders are {employee_register.approval_counter}")
-        print(f"The number of pending orders are {employee_register.pending_counter}")
-        print(f"The number of declined orders are {employee_register.decline_counter}")
+            Registry.decline_counter += 1
+        print(f"The number of approved orders are {Registry.approval_counter}")
+        print(f"The number of pending orders are {Registry.pending_counter}")
+        print(f"The number of declined orders are {Registry.decline_counter}")
 
     def display_all(self):
-        employee_register.order_count += 1
-        print(employee_register.registry_info)
-        print(employee_register.receipt)
-        print(f"The number of approved orders are {employee_register.approval_counter}")
-        print(f"The number of pending orders are {employee_register.pending_counter}")
-        print(f"The number of declined orders are {employee_register.decline_counter}")
+        Registry.order_count += 1
+        print(Registry.registry_info)
+        print(Registry.receipt)
+        print(f"The number of approved orders are {Registry.approval_counter}")
+        print(f"The number of pending orders are {Registry.pending_counter}")
+        print(f"The number of declined orders are {Registry.decline_counter}")
 
 
-    register1 = employee_register("Jan 1", "Roy", "abc12")
+register1 = Registry("Jan 1", "Roy", "abc12")
+print(register1)
 
 
 
