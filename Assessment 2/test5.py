7b9d8d90cf703bbb4c2abde5fcfc74b39aa74115
S-pen-cer <20260814@mywhitecliffe.com>
2026-06-29 10:07:14 +1200

    Add files via upload
    This runs into a weird error. It doesn't recognize the class name, even though nothing seems wrong. Because of that, it stops right at the start of running.

--- Assessment 2/test5.py
+++ Assessment 2/test5.py
@@ -4,6 +4,9 @@ class employee_register:
     order_count = 0
     registry_info = []
     receipt = []
+    approval_counter = 0
+    decline_counter = 0
+    pending_counter = 0
     def __init__(self, date, name, id, status):
         self.date = date
         self.name = name
@@ -31,11 +34,28 @@ class employee_register:
         print(employee_register.Cost, self.status)
         employee_register.registry_info = [self.date, self.name, self.id, self.status]
 
-    def
-    def display_all:
+    def order_stats(self):
+        if self.status == "approved":
+            employee_register.approval_counter += 1
+        if self.status == "pending":
+            employee_register.approval_counter += 1
+        if self.status == "declined":
+            employee_register.decline_counter += 1
+        print(f"The number of approved orders are {employee_register.approval_counter}")
+        print(f"The number of pending orders are {employee_register.pending_counter}")
+        print(f"The number of declined orders are {employee_register.decline_counter}")
+
+    def display_all(self):
         employee_register.order_count += 1
         print(employee_register.registry_info)
         print(employee_register.receipt)
+        print(f"The number of approved orders are {employee_register.approval_counter}")
+        print(f"The number of pending orders are {employee_register.pending_counter}")
+        print(f"The number of declined orders are {employee_register.decline_counter}")
+
+
+    register1 = employee_register("Jan 1", "Roy", "abc12")
+
 
 
 
