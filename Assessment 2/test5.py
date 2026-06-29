f8e56810a3ea91ca17fb303d261bb74ed6d58e08
S-pen-cer <20260814@mywhitecliffe.com>
2026-06-29 16:48:51 +1200

    Add files via upload
    Key changes for this version:
         Moved the majority of print statements to the display_all method for cleaner output (old statements placed after #; they are still present in the code)
         Changed some print() statements to print(f{}) for presentation
    
    Seems to be working smoother than before. Might be time for some more additions if possible or necessary.

--- Assessment 2/test5.py
+++ Assessment 2/test5.py
@@ -33,7 +33,7 @@ class Registry:
     def update_status(self):
         if Registry.total_cost < 500:
             self.status = "approved"
-        print(Registry.total_cost, self.status)
+        print(self.status)
         #Registry.registry_info = [self.date, self.name, self.id, self.status]
         #print(Registry.registry_info)
 
@@ -41,7 +41,7 @@ class Registry:
         new_status = input("Please enter new status")
         self.status = new_status
         Registry.registry_info = [self.date, self.name, self.id, self.status]
-        print(Registry.registry_info)
+        #print(Registry.registry_info)
         Registry.order_count += 1
         #Registry.registry_info.remove(self.status)
         #Registry.registry_info.append(new_status)
@@ -53,7 +53,7 @@ class Registry:
         id3 = Registry.order_count
         full_id = id1 + id2 + str(id3)
         Registry.assigned_id = full_id
-        print(Registry.assigned_id)
+        #print(Registry.assigned_id)
 
     def order_stats(self):
         if self.status == "approved":
@@ -62,18 +62,19 @@ class Registry:
             Registry.pending_counter += 1
         if self.status == "declined":
             Registry.decline_counter += 1
-        print(f"The number of approved orders are {Registry.approval_counter}")
-        print(f"The number of pending orders are {Registry.pending_counter}")
-        print(f"The number of declined orders are {Registry.decline_counter}")
-'''
+        #print(f"The number of approved orders are {Registry.approval_counter}")
+        #print(f"The number of pending orders are {Registry.pending_counter}")
+        #print(f"The number of declined orders are {Registry.decline_counter}")
+
     def display_all(self):
         #Registry.order_count += 1
         print(Registry.registry_info)
         print(Registry.receipt)
+        print(f"The Order ID is {Registry.assigned_id}")
         print(f"The number of approved orders are {Registry.approval_counter}")
         print(f"The number of pending orders are {Registry.pending_counter}")
         print(f"The number of declined orders are {Registry.decline_counter}")
-'''
+
 
 
 
@@ -89,7 +90,7 @@ Registry.update_status(register1)
 Registry.manual_update(register1)
 Registry.assign_id(register1)
 Registry.order_stats(register1)
-#Registry.display_all(register1)
+Registry.display_all(register1)
 
 
 
