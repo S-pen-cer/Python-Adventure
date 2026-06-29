d8f2374e8020423324506c83e9fca3b6a8aa4e23
S-pen-cer <20260814@mywhitecliffe.com>
2026-06-29 15:12:32 +1200

    Add files via upload
    Some more additions since last version. Added manual_update to allow manual inputs whenever changing status, but not sure if it's working.
    Because of that, checked to see if code is still running properly at a certain point (hence the ''' ''' in one section of the code).
    Might be something wrong after this...

--- Assessment 2/test5.py
+++ Assessment 2/test5.py
@@ -4,6 +4,7 @@ class Registry:
     order_count = 0
     registry_info = []
     receipt = []
+    assigned_id = ""
     approval_counter = 0
     decline_counter = 0
     pending_counter = 0
@@ -18,7 +19,8 @@ class Registry:
         id1 = self.name[0:3]
         id2 = self.id[-3:]
         full_id = id1 + id2
-        print(full_id)
+        Registry.assigned_id = full_id
+        print(Registry.assigned_id)
 
     def get_order(self):
         item = input("Please enter product name")
@@ -33,23 +35,30 @@ class Registry:
         cost = price * quantity
         Registry.total_cost = cost
         Registry.receipt = [item, Registry.total_cost]
+        print(Registry.receipt)
 
     def update_status(self):
         if Registry.total_cost < 500:
             self.status = "approved"
         print(Registry.total_cost, self.status)
         Registry.registry_info = [self.date, self.name, self.id, self.status]
+        print(Registry.registry_info)
+'''
+    def manual_update(self):
+        new_status = input("Please enter new status")
+        self.status = new_status
+        print(Registry.registry_info)
 
     def order_stats(self):
         if self.status == "approved":
             Registry.approval_counter += 1
         if self.status == "pending":
-            Registry.approval_counter += 1
+            Registry.pending_counter += 1
         if self.status == "declined":
             Registry.decline_counter += 1
-        print(f"The number of approved orders are {Registry.approval_counter}")
-        print(f"The number of pending orders are {Registry.pending_counter}")
-        print(f"The number of declined orders are {Registry.decline_counter}")
+        #print(f"The number of approved orders are {Registry.approval_counter}")
+        #print(f"The number of pending orders are {Registry.pending_counter}")
+        #print(f"The number of declined orders are {Registry.decline_counter}")
 
     def display_all(self):
         Registry.order_count += 1
@@ -58,6 +67,8 @@ class Registry:
         print(f"The number of approved orders are {Registry.approval_counter}")
         print(f"The number of pending orders are {Registry.pending_counter}")
         print(f"The number of declined orders are {Registry.decline_counter}")
+
+'''
 
 
 register1 = Registry("Jan 1", "Roy", "abc12", "")
@@ -69,6 +80,7 @@ print(register1.status)
 Registry.assign_id(register1)
 Registry.get_order(register1)
 Registry.update_status(register1)
+Registry.manual_update(register1)
 Registry.order_stats(register1)
 Registry.display_all(register1)
 
